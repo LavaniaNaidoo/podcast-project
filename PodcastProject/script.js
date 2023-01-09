bdf78c9159cbad41f87849aea6fad5d554e68fe0
LavaniaNaidoo <lavania987@gmail.com>
2023-01-09 15:04:39 +0200

    Fixed major issues

--- PodcastProject/script.js
+++
@@ -1,4 +0,0 @@
-import './components/podcasts-app-components.js'
-import './components/view-list-components.js'
-import './components/single-view-components.js'
-import './components/control-component.js'
